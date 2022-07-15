b5a6a663814f172508736780220f263f31b8dc58
Miguelangel <miguelangel.dgonzalez@gmail.com>
2022-07-15 17:53:24 -0400

    Firs review

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Contact from '../pages/Contactanos.jsx';
 import Layout from '../containers/Layout.jsx';
 import Encargo from '../pages/Encargo.jsx';
 import Home from '../pages/Home.jsx';
@@ -12,6 +13,8 @@ const App = () => {
                 <Routes>
                     <Route exact path='/' element={<Home />}/>
                     <Route exact path='/encargo' element={<Encargo />} />
+                    <Route exact path='/contactanos' element={<Contact />} />
+                    <Route path='/*' element={<Home />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
