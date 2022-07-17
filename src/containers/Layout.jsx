a2a65d27376676b385a690deef5413c00cc6e2f3
Miguelangel <miguelangel.dgonzalez@gmail.com>
2022-07-17 16:27:24 -0400

    [FIX] set on top

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {useLocation} from 'react-router-dom';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,8 +9,15 @@ import '../styles/_vars.css';
 import '../styles/Layout.css';
 
 const Layout = ({children}) => {
+    const {pathname} = useLocation();
+
+    const setOnTop = path => {
+        window.scrollTo(0,0);
+    }
+
     return (
         <div className='layout'>
+            {setOnTop(pathname)}
             <Header />
             {children}
             <Footer />
